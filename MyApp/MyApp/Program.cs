4cecbb0e0287a904e7c84628091f31276b566054
Imperia-Aladeras <aladeras@imperiascm.com>
2025-10-17 11:26:06 +0200

    Configure Entity Framework Core for SQL Server

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,21 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+
 namespace MyApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
